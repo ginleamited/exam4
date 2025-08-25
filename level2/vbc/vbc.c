7b8af2c67f96832e29bf00c47d1f33cd9df36533
Jingyang Lin <ginleamited@BIGMAC.local>
2025-08-25 16:31:12 +0200

    ADD

--- level2/vbc/vbc.c
+++ level2/vbc/vbc.c
@@ -1,165 +1,106 @@
-#include <stdio.h>
-#include <stdlib.h>    // MOD: on utilise <stdlib.h> pour calloc/free au lieu de <malloc.h>
-#include <ctype.h>
+#include "vbc.h"
 
-typedef struct node {
-    enum { 
-        ADD, 
-        MULTI,
-        VAL 
-    }   type;
-    int           val;
-    struct node   *l;
-    struct node   *r;
-} node;
+static char	*exp;
+static int	pos;
 
-node *new_node(node n)
+void	utils(int action, const char *msg, char c)
 {
-    node *ret = calloc(1, sizeof(*ret));  // MODIFIED: sizeof(*ret) instead sizeof(n)
-    if (!ret)
-        return NULL;
-    *ret = n;
-    return ret;
+	if (action == 0)
+	{
+		while (exp[pos] == ' ')
+			pos++;
+	}
+	else if (action == 1)
+	{
+		if (msg)
+			printf("%s\n", msg);
+		exit(1);
+	}
+	else if (action == 2)
+	{
+		printf("Unexpected token '%c'\n", c);
+		exit(1);
+	}
 }
 
-void destroy_tree(node *n)
+int	parse_factor(void)
 {
-    if (!n)
-        return;
-    if (n->type != VAL) {
-        destroy_tree(n->l);
-        destroy_tree(n->r);
-    }
-    free(n);
+	int	result;
+
+	utils(0, NULL, 0);
+	if (!exp[pos])
+		utils(1, "Unexpected end of input", 0);
+	if (isdigit(exp[pos]))
+	{
+		result = exp[pos] - '0';
+		pos++;
+		return result;
+	}
+	else if (exp[pos] == '(')
+	{
+		pos++;
+		result = handle_plus();
+		utils(0, NULL, 0);
+		if (exp[pos] != ')')
+			utils(1, "Unexpected end of input", 0);
+		pos++;
+		return result;
+	}
+	else
+		utils(2, NULL, exp[pos]);
+	return 0;
 }
 
-void unexpected(char c)
+int	handle_mult(void)
 {
-    if (c)
-        printf("Unexpected token '%c'\n", c);
-    else
-        printf("Unexpected end of input\n");  // MODIFIED: correct message according to the subject
+	int result = parse_factor();
+
+	while (1)
+	{
+		utils(0, NULL, 0);
+		if (exp[pos] == '*')
+		{
+			pos++;
+			result *= parse_factor();
+		}
+		else
+			break;
+	}
+	return result;
 }
 
-/* MODIFIED: accept/expect remises à la signature d’origine, sans global */
-int accept(char **s, char c)
+int	handle_plus(void)
 {
-    if (**s == c) {
-        (*s)++;
-        return 1;
-    }
-    return 0;
-}
+	int result = handle_mult();
 
-int expect(char **s, char c)
-{
-    if (accept(s, c))
-        return 1;
-    unexpected(**s);
-    return 0;
-}
-
-/* ADDED: déclarations des fonctions de parsing récursif */
-static node *parse_expr_r(char **s);
-static node *parse_term   (char **s);
-static node *parse_factor (char **s);
-
-/* ADDED: parsing d’un facteur (chiffre ou parenthèse) */
-static node *parse_factor(char **s)
-{
-    if (isdigit((unsigned char)**s)) {
-        node n = { .type = VAL, .val = **s - '0', .l = NULL, .r = NULL };
-        (*s)++;
-        return new_node(n);
-    }
-    if (accept(s, '(')) {
-        node *e = parse_expr_r(s);
-        if (!e)
-            return NULL;
-        if (!expect(s, ')')) {          // MOD: vérification de la parenthèse fermante
-            destroy_tree(e);
-            return NULL;
-        }
-        return e;
-    }
-    unexpected(**s);
-    return NULL;
-}
-
-/* ADDED: parsing d’un terme (multiplications) */
-static node *parse_term(char **s)
-{
-    node *left = parse_factor(s);
-    if (!left)
-        return NULL;
-    while (accept(s, '*')) {
-        node *right = parse_factor(s);
-        if (!right) {
-            destroy_tree(left);
-            return NULL;
-        }
-        node n = { .type = MULTI, .l = left, .r = right };
-        left = new_node(n);
-        if (!left)
-            return NULL;
-    }
-    return left;
-}
-
-/* ADDED: parsing d’une expression (additions) */
-/* THIS FUNCTION IS A COPY PASTE OF PARSE_TERM, YOU JUST HAVE TO REPLACE '*' by '+' !!!!!*/
-static node *parse_expr_r(char **s)
-{
-    node *left = parse_term(s);
-    if (!left)
-        return NULL;
-    while (accept(s, '+')) {
-        node *right = parse_term(s);
-        if (!right) {
-            destroy_tree(left);
-            return NULL;
-        }
-        node n = { .type = ADD, .l = left, .r = right };
-        left = new_node(n);
-        if (!left)
-            return NULL;
-    }
-    return left;
-}
-
-/* MODIFIED: parse_expr initialise un pointeur local et vérifie la fin de la chaîne */
-node *parse_expr(char *s)
-{
-    char *p = s;
-    node *ret = parse_expr_r(&p);
-    if (!ret)
-        return NULL;
-    if (*p) {                          
-        unexpected(*p);
-        destroy_tree(ret);
-        return NULL;
-    }
-    return ret;
-}
-
-int eval_tree(node *tree)
-{
-    switch (tree->type) {
-        case ADD:   return eval_tree(tree->l) + eval_tree(tree->r);
-        case MULTI: return eval_tree(tree->l) * eval_tree(tree->r);
-        case VAL:   return tree->val;
-    }
-    return 0;  // should no happen, but who know's ?
+	while (1)
+	{
+		utils(0, NULL, 0);
+		if (exp[pos] == '+')
+		{
+			pos++;
+			result += handle_mult();
+		}
+		else
+			break;
+	}
+	return result;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        return 1;
-    node *tree = parse_expr(argv[1]);
-    if (!tree)
-        return 1;
-    printf("%d\n", eval_tree(tree));
-    destroy_tree(tree);
-    return 0;
+	int	result;
+
+	if (argc != 2)
+		exit(1);
+	exp = argv[1];
+	pos = 0;
+	result = handle_plus();
+	utils(0, NULL, 0);
+	if (exp[pos] == ')')
+		utils(2, NULL, ')');
+	if (exp[pos])
+		utils(2, NULL, exp[pos]);
+	printf("%d\n", result);
+	return 0;
 }
--- level2/vbc/vbc1.c
+++ level2/vbc/vbc1.c
@@ -1,157 +1,171 @@
 #include <stdio.h>
-// #include <malloc.h>
+#include <stdlib.h>    // MOD: on utilise <stdlib.h> pour calloc/free au lieu de <malloc.h>
 #include <ctype.h>
-#include <stdlib.h>
 
 typedef struct node {
-    enum {
-        ADD,
+    enum { 
+        ADD, 
         MULTI,
-        VAL
+        VAL 
     }   type;
-    int val;
-    struct node *l;
-    struct node *r;
-}   node;
+    int           val;
+    struct node   *l;
+    struct node   *r;
+} node;
 
-node *parse_expr(char **s);
-node *parse_factor(char **s);
-node *parse_term(char **s);
-
-node    *new_node(node n)
+node *new_node(node n)
 {
-    node *ret = calloc(1, sizeof(n));
+    node *ret = calloc(1, sizeof(*ret));  // MODIFIED: sizeof(*ret) instead sizeof(n)
     if (!ret)
-        return (NULL);
+        return NULL;
     *ret = n;
-    return (ret);
+    return ret;
 }
 
-void    destroy_tree(node *n)
+void destroy_tree(node *n)
 {
     if (!n)
-        return ;
-    if (n->type != VAL)
-    {
+        return;
+    if (n->type != VAL) {
         destroy_tree(n->l);
         destroy_tree(n->r);
     }
     free(n);
 }
 
-void    unexpected(char c)
+void unexpected(char c)
 {
     if (c)
         printf("Unexpected token '%c'\n", c);
     else
-        printf("Unexpexted end of input\n");
-    exit(1);
+        printf("Unexpected end of input\n");  // MODIFIED: correct message according to the subject
 }
 
+/* MODIFIED: accept/expect remises à la signature d’origine, sans global */
 int accept(char **s, char c)
 {
-    if (**s == c)
-    {
+    if (**s == c) {
         (*s)++;
-        return (1);
+        return 1;
     }
-    return (0);
+    return 0;
 }
 
 int expect(char **s, char c)
 {
     if (accept(s, c))
-        return (1);
+        return 1;
     unexpected(**s);
-    return (0);
+    return 0;
 }
 
+/*-------------------------------UNDER-----------------------------------------*/
+/*-------------------------------UNDER-----------------------------------------*/
 
-int eval_tree(node *tree)
+/* ADDED: déclarations des fonctions de parsing récursif */
+static node *parse_expr_r(char **s);
+static node *parse_term   (char **s);
+static node *parse_factor (char **s);
+
+/* ADDED: parsing d’un facteur (chiffre ou parenthèse) */
+static node *parse_factor(char **s)
 {
-    switch (tree->type)
-    {
-        case ADD:
-            return (eval_tree(tree->l) + eval_tree(tree->r));
-        case MULTI:
-            return (eval_tree(tree->l) * eval_tree(tree->r));
-        case VAL:
-            return (tree->val);
-    }
-    return (0);
-}
-
-node *parse_expr(char **s) {
-    node *left = parse_term(s);
-    if (!left)
-        return (NULL);
-    while (accept(s, '+')) {
-        node *right = parse_term(s);
-        if (!right) {
-            destroy_tree(left);
-            return (NULL);
-        }
-        node n = {ADD, 0, left, right};
-        left = new_node(n);
-        if (!left) {
-            destroy_tree(right);
-            return (NULL);
-        }
-    }
-    return (left);
-}
-
-node *parse_factor(char **s) {
-    if (isdigit(**s)) {
-        node n = {VAL, **s - '0', NULL, NULL};
+    if (isdigit((unsigned char)**s)) {
+        node n = { .type = VAL, .val = **s - '0', .l = NULL, .r = NULL };
         (*s)++;
-        return (new_node(n));
+        return new_node(n);
     }
     if (accept(s, '(')) {
-        node *n = parse_expr(s);
-        if (!expect(s, ')')) {
-            destroy_tree(n);
-            return (NULL);
+        node *e = parse_expr_r(s);
+        if (!e)
+            return NULL;
+        if (!expect(s, ')')) {          // MOD: vérification de la parenthèse fermante
+            destroy_tree(e);
+            return NULL;
         }
-        return (n);
+        return e;
     }
     unexpected(**s);
-    return (NULL);
+    return NULL;
 }
 
-node *parse_term(char **s) {
+/* ADDED: parsing d’un terme (multiplications) */
+static node *parse_term(char **s)
+{
     node *left = parse_factor(s);
     if (!left)
-        return (NULL);
+        return NULL;
     while (accept(s, '*')) {
         node *right = parse_factor(s);
         if (!right) {
             destroy_tree(left);
-            return (NULL);
+            return NULL;
         }
-        node n = {MULTI, 0, left, right};
+        node n = { .type = MULTI, .l = left, .r = right };
         left = new_node(n);
-        if (!left) {
-            destroy_tree(right);
-            return (NULL);
-        }
+        if (!left)
+            return NULL;
     }
-    return (left);
+    return left;
+}
+
+/* ADDED: parsing d’une expression (additions) */
+/* THIS FUNCTION IS A COPY PASTE OF PARSE_TERM, YOU JUST HAVE TO REPLACE '*' by '+' !!!!!*/
+static node *parse_expr_r(char **s)
+{
+    node *left = parse_term(s);
+    if (!left)
+        return NULL;
+    while (accept(s, '+')) {
+        node *right = parse_term(s);
+        if (!right) {
+            destroy_tree(left);
+            return NULL;
+        }
+        node n = { .type = ADD, .l = left, .r = right };
+        left = new_node(n);
+        if (!left)
+            return NULL;
+    }
+    return left;
+}
+
+/* MODIFIED: parse_expr initialise un pointeur local et vérifie la fin de la chaîne */
+node *parse_expr(char *s)
+{
+    char *p = s;
+    node *ret = parse_expr_r(&p);
+    if (!ret)
+        return NULL;
+    if (*p) {                          
+        unexpected(*p);
+        destroy_tree(ret);
+        return NULL;
+    }
+    return ret;
+}
+
+/*-------------------------------UPPER-----------------------------------------*/
+/*-------------------------------UPPER-----------------------------------------*/
+
+int eval_tree(node *tree)
+{
+    switch (tree->type) {
+        case ADD:   return eval_tree(tree->l) + eval_tree(tree->r);
+        case MULTI: return eval_tree(tree->l) * eval_tree(tree->r);
+        case VAL:   return tree->val;
+    }
+    return 0;  // should no happen, but who know's ?
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
-        return (1);
-    char *s = argv[1];
-    node *tree = parse_expr(&s);
-    if (!tree || *s) {
-        if (*s)
-            unexpected(*s);
-        else
-            unexpected(0);
-        return (1);
-    }
+        return 1;
+    node *tree = parse_expr(argv[1]);
+    if (!tree)
+        return 1;
     printf("%d\n", eval_tree(tree));
     destroy_tree(tree);
+    return 0;
 }
