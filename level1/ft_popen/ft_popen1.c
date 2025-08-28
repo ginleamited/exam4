cc3bde0ee135c715f36f80734b86ecfefaeb544e
Jingyang Lin <ginleamited@BIGMAC.local>
2025-08-28 19:51:01 +0200

    ADD

--- level1/ft_popen/ft_popen1.c
+++ level1/ft_popen/ft_popen1.c
@@ -1,39 +1,53 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int	ft_popen(const char *file, char *const argv[], char type)
+static void	child(int *fd, const char *file, char *const argv[], char type)
 {
-	if (!file || !argv || (type != 'r' && type != 'w'))
-		return -1;
-
-	int fd[2];
-
-	pipe(fd);
 	if (type == 'r')
 	{
-		if (fork() == 0)
-		{
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[0]);
-			close(fd[1]);
-			execvp(file, argv);
-			exit (-1);
-		}
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+	}
+	execvp(file, argv);
+	exit(127);
+}
+
+static int	parent(int *fd, char type)
+{
+	if (type == 'r')
+	{
 		close(fd[1]);
 		return (fd[0]);
 	}
-	if (type == 'w')
+	else
 	{
-		if (fork() == 0)
-		{
-			dup2(fd[0], STDIN_FILENO);
-			close(fd[0]);
-			close(fd[1]);
-			execvp(file, argv);
-			exit (-1);
-		}
 		close(fd[0]);
 		return (fd[1]);
 	}
-	return -1;
+}
+
+int	ft_popen(const char *file, char *const argv[], char type)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (!file || !argv || (type != 'r' && type != 'w') || pipe(fd) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		return (-1);
+	}
+	if (pid == 0)
+		child(fd, file, argv, type);
+	return (parent(fd, type));
 }
